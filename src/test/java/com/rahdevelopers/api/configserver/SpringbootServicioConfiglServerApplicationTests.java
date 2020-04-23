ace49cc9f71b6df68abe06af9fa2d2e55ff5c613
Robin Arellano <robinarehen@gmail.com>
2020-04-23 14:04:00 +0200

    commit initial

---
+++ src/test/java/com/rahdevelopers/api/configserver/SpringbootServicioConfiglServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahdevelopers.api.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfiglServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
